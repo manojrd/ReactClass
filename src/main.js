fe3e82041eefb4ada3f65b232c2cbcec93f5764a
AAKASH HANDA <AAKASH HANDA>
2017-02-02 06:56:01 +0530

    a

---
+++ src/main.js
@@ -0,0 +1,16 @@
+
+/*var React = require('react');
+
+var Router = require('react-router');
+var routes = require('./routes');
+
+Router.run(routes,  function(Handler){
+	React.render(<Handler />, document.getElementById('app'));
+});
+*/
+//2
+$=jQuery=require('jQuery');
+//1
+var App = console.log('hello hi class from browserify');
+//1
+module.exports =App;
